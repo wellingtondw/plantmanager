b3da6eff706beffae9c15459406ff143199a980f
Wellington de Lima <wellington.map@hotmail.com>
2021-04-21 21:39:55 -0300

    add fonts and end home

--- App.tsx
+++ App.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost'
+import AppLoading from 'expo-app-loading'
+
 import { Welcome } from "./src/pages/Welcome";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  })
+
+  if(!fontsLoaded) {
+    return (
+      <AppLoading />
+    )
+  }
+
   return (
     <Welcome />
   );
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,39 +5,43 @@ import {
   Text, 
   StyleSheet, 
   TouchableOpacity, 
-  Dimensions 
+  Dimensions, 
+  View
 } from "react-native";
 import { Feather } from '@expo/vector-icons'
 
 import wateringImg from '../assets/watering.png'
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {'\n'}
-        suas plantas de {'\n'}
-        forma fácil
-      </Text>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {'\n'}
+          suas plantas de {'\n'}
+          forma fácil
+        </Text>
 
-      <Image 
-        source={wateringImg} 
-        style={styles.image}
-        resizeMode='contain'
-      />
+        <Image 
+          source={wateringImg} 
+          style={styles.image}
+          resizeMode='contain'
+        />
 
-      <Text  style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas. 
-        Nós cuidamos de lembrar você sempre que precisar.
-      </Text>
+        <Text  style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. 
+          Nós cuidamos de lembrar você sempre que precisar.
+        </Text>
 
-      <TouchableOpacity 
-        activeOpacity={0.7} 
-        style={styles.button}
-      >
-        <Feather name='chevron-right' style={styles.buttonIcon} />
-      </TouchableOpacity>
+        <TouchableOpacity 
+          activeOpacity={0.7} 
+          style={styles.button}
+        >
+          <Feather name='chevron-right' style={styles.buttonIcon} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
@@ -45,16 +49,21 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
+    paddingHorizontal: 20
   },
   title: {
-    fontSize: 32,
-    lineHeight: 38,
+    fontSize: 28,
+    lineHeight: 34,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
-    marginTop: 38
+    marginTop: 38,
+    fontFamily: fonts.heading
   },
   image: {
     height: Dimensions.get('window').width * 0.7
@@ -64,7 +73,8 @@ const styles = StyleSheet.create({
     fontSize: 18,
     lineHeight: 25,
     paddingHorizontal: 20,
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.text
   },
   button: {
     backgroundColor: colors.green,
